40c0dbe9d79aa9a1da089054123a670d7c738006
Vy Dao <vynguyendao@gmail.com>
2021-10-06 22:54:47 +0700

    Managing configuration settings by command-line flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	http.ListenAndServe(":8080", mux)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
 }
